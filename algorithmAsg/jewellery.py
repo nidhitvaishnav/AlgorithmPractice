034c50c8414b46d882adf249a0916e5b13e94c27
nidhitvaishnav <nidhitvaishnav@gmail.com>
2018-04-01 00:22:52 -0500

    working on min limit - under progress

--- algorithmAsg/jewellery.py
+++ algorithmAsg/jewellery.py
@@ -23,11 +23,25 @@ def rodCutting(w, size, inputData):
         for i in range(1, j+1):
             # debug
             print(i, inputData[i,2])
-            print("i={}, j={}, max({}, {}+{})".format(i, j,q, inputData[i,2], r[j-i]))
+           # print("i={}, j={}, max({}, {}+{})".format(i, j,q, inputData[i,2], r[j-i]))
             # debug -ends
-            if inputData[i,4]>0:
+            if inputData[i,4]>=0:
                 oldQ=q
-                q=max(q, inputData[i,2]+r[j-i])
+                if i*inputData[i,3] > j:
+                    temp=inputData[i,2]*inputData[i,3]+r[j-i*inputData[i,3]]
+                    q=max(q,temp )
+                    print("i={},j={},max({},{}+{})".format(i,j,q,inputData[i,2]*inputData[i,3],r[j-i*inputData[i,3]]))
+                else:
+                    min=inputData[i,3]
+                    if min==0:
+                        min+=1
+                    while(min>0):
+                        if(min*i<=j):
+                            break
+                        min-=1
+                    temp=inputData[i,2]*min+r[j-i*min]
+                    q=max(q,temp) 
+                    print("i={}, j={}, max({},{}+{}".format(i,j,q,inputData[i,2]*min,r[j-i*min]))      
                 if oldQ!=q:
                     cutIndex = i
         #for i -ends
@@ -67,7 +81,7 @@ if __name__ == '__main__':
 #     inputData = np.array([[0,0,0,0,0,0,0],[1,2,10,8,10,2,2],[2,2,10,8,10,2,4]])
     inputData = np.array([[0,0,0,0,0],\
                           [1,1,1,0,10],\
-                          [2,2,5,0,1],\
+                          [2,2,5,3,10],\
                           [3,3,8,0,10],\
                           [4,4,9,0,10],\
                           [5,5,10,0,10],\
